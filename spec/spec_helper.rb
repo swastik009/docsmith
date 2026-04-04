0ac24d7496714ec2d7863f349971bf42a59f6aa2
swastik009 <swastik.thapaliya@gmail.com>
2026-04-05 01:54:56 +0545

    test(infra): add .rspec flags and spec_helper with SQLite + FactoryBot setup
    Co-authored-by: Swastik <swastik.thapaliya@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,33 @@
 # frozen_string_literal: true
 
+require "active_record"
+require "active_support"
+require "active_support/core_ext/numeric/time"
+require "factory_bot"
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+ActiveRecord::Base.logger = nil
+
 require "docsmith"
 
+require_relative "support/schema"
+require_relative "support/models"
+require_relative "support/factories"
+
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.include FactoryBot::Syntax::Methods
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.around(:each) do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
   end
+
+  # Reset global Docsmith config between examples so hooks/settings don't bleed.
+  config.before(:each) { Docsmith.reset_configuration! }
+
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+  config.disable_monkey_patching!
+  config.example_status_persistence_file_path = ".rspec_status"
 end
